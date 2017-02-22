ababcf99e7f6980fbe06e941358877d68cca4e5f
Akiicat <aaaa1379@gmail.com>
2017-02-23 01:54:02 +0800

    add image to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
+  # cocoon
   has_many :directions, inverse_of: :book
-
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
+
+  # paperclip
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
